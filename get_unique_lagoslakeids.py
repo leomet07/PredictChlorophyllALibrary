3573a445849f5bff414dbff5cb3bc948443ad245
leomet07 <leomet07@gmail.com>
2024-12-13 18:57:01 -0500

    quick util script to get lagoslakeids

---
+++ get_unique_lagoslakeids.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+nys_lagosid_path = "ccri_lakes_withLagosID.csv"
+nys_lagosid = pd.read_csv(nys_lagosid_path)
+
+nys_lagosid = nys_lagosid[["lagoslakei"]]  # select relevant columns from lagosid
+
+
+nys_lagosid = nys_lagosid.drop_duplicates()
+nys_lagosid.to_csv("insitulakeids.csv", index=False)
+print(nys_lagosid)
+
+import geopandas as gp
+
+gp.GeoDataFrame()
+shp = gp.GeoDataFrame.from_file("LAGOS_NY_4ha_Polygons.shp")
+
+shp = shp[["lagoslakei"]]
+shp = shp.drop_duplicates()
+
+shp.to_csv("all_nys_lakes_lagoslakeids.csv", index=False)
+
+print(shp)
