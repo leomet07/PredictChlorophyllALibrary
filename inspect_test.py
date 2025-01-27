ac71414ecb87e8c11008a861c13c4d16aa81a4ed
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-01-27 12:17:32 -0500

    create script to veirfy that there are no bugged blank downloads in a downloaded tif dir

--- inspect_test.py
+++ inspect_test.py
@@ -3,29 +3,30 @@ import rasterio
 
 out_file = input("Enter the path of a tif to inspect: ") or "out/prospect.tif"
 
-with rasterio.open(out_file) as src:
-    # Read the number of bands and the dimensions
-    num_bands = src.count
-    height = src.height
-    width = src.width
-    print(src.profile)
+def inspect(filepath: str):
+    with rasterio.open(filepath) as src:
+        # Read the number of bands and the dimensions
+        num_bands = src.count
+        height = src.height
+        width = src.width
+        print(src.profile)
 
-    print(f"Number of bands: {num_bands}")
-    print(f"Dimensions: {width} x {height}")
+        print(f"Number of bands: {num_bands}")
+        print(f"Dimensions: {width} x {height}")
 
-    # Read the entire image into a numpy array (bands, height, width)
-    img = src.read()
-    print("hi2")
-    # Display each band separately
-    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+        # Read the entire image into a numpy array (bands, height, width)
+        img = src.read()
+        # Display each band separately
+        fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
 
-    for i, ax in enumerate(axes.flatten()):
-        if i < num_bands:
-            ax.imshow(img[i, :, :], cmap="gray")  # Display each band separately
-            ax.set_title(f"Band {i+1}")
-            ax.axis("off")
+        for i, ax in enumerate(axes.flatten()):
+            if i < num_bands:
+                ax.imshow(img[i, :, :], cmap="gray")  # Display each band separately
+                ax.set_title(f"Band {i+1}")
+                ax.axis("off")
 
-            print(img[i, :, :])
+        plt.tight_layout()
+        plt.show()
 
-    plt.tight_layout()
-    plt.show()
+if __name__ == "__main__":
+    inspect(out_file)                
---
+++ verify_no_empty_images_in_folder.py
@@ -0,0 +1,32 @@
+import os
+import rasterio
+import numpy as np
+from tqdm import tqdm
+from pprint import pprint
+
+def is_list_valid(filepath: str):
+    with rasterio.open(filepath) as src:
+        # Read the entire image into a numpy array (bands, height, width)
+        img = src.read()
+
+        flattened_img = img.flatten()
+
+        if np.all(flattened_img == np.float32("-inf")):
+            return False
+        return True
+    
+    return invalid_paths
+
+invalid_paths = []
+
+satellite_tif_folder = input("Enter the path to the downloaded tifs you want to inspect: ")
+
+paths = os.listdir(satellite_tif_folder)
+
+for path in tqdm(paths):
+    filepath = os.path.join(satellite_tif_folder, path)
+    if not is_list_valid(filepath):
+        invalid_paths.append(filepath)
+
+pprint(invalid_paths)
+print("# of invalid paths: ", len(invalid_paths))
